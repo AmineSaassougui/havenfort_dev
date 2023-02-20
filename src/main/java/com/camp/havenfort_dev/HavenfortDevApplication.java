7a40487e8bcc53a233b0082f00678101cb91bd5c
Amineesprit007 <mohamedamine.saassougui@esprit.tn>
2023-02-20 20:56:36 +0000

    firstproject

---
+++ src/main/java/com/camp/havenfort_dev/HavenfortDevApplication.java
@@ -0,0 +1,13 @@
+package com.camp.havenfort_dev;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HavenfortDevApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HavenfortDevApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/camp/havenfort_dev/HavenfortDevApplicationTests.java
@@ -0,0 +1,13 @@
+package com.camp.havenfort_dev;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HavenfortDevApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
