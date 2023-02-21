86f68a9e6160f3977f0fea118280c017a570e17e
Amineesprit007 <mohamedamine.saassougui@esprit.tn>
2023-02-21 13:12:28 +0000

    add methods done for my task

--- src/main/java/com/camp/havenfort_dev/services/ICategoryServices.java
+++ src/main/java/com/camp/havenfort_dev/services/ICategoryServices.java
@@ -1,7 +1,18 @@
 package com.camp.havenfort_dev.services;
 
 import com.camp.havenfort_dev.entities.Category;
+import com.camp.havenfort_dev.entities.Promotion;
+import com.camp.havenfort_dev.entities.Shop;
+import com.camp.havenfort_dev.entities.Tools;
 
 public interface ICategoryServices {
     Category addCategory(Category category);
+
+    Category updateCategory(Category category);
+
+    Promotion addPromotion(Promotion promotion);
+
+    Shop addShop(Shop shop);
+
+    Tools addTools(Tools tools);
 }
