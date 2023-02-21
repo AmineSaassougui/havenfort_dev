7e41f82da6c1ecf970761d1d464725517707c853
jedidi-tasnim <tasnim.jedidi@esprit.tn>
2023-02-21 14:49:25 +0100

    Services

---
+++ src/main/java/services/EventService.java
@@ -0,0 +1,32 @@
+package services;
+
+import entities.Event;
+
+import java.util.List;
+
+public class EventService implements IEventService{
+    @Override
+    public Event addEvent(Event event) {
+        return null;
+    }
+
+    @Override
+    public List<Event> findAllEvent() {
+        return null;
+    }
+
+    @Override
+    public Event updateEvent(Event event) {
+        return null;
+    }
+
+    @Override
+    public Event findById(Long id) {
+        return null;
+    }
+
+    @Override
+    public void deleteEvent(Long id) {
+
+    }
+}
